05745388863089aef633d6bf3d3d0daf547f3c92
CapitanSur <78511846+CapitanSur@users.noreply.github.com>
2021-02-03 20:49:52 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+// pregunta al usuario su nombre
+const nombre = prompt('Cual es tu nombre?');
+//toma el nombre y lo muetra en pantalla
+
+document.querySelector('.contenido').innerHTML = `${nombre} esta aprendiendo Javascrips moderno`
+// comentario de linea
+/* comenta varias lineas
+*/ 
+//asigna el valor hacia la variable producto
+const producto = 'monitos 24 pulgadas';
+
+//el punto y coma termina la linea de codigo
+//no es necesario pero es un buen habito
+console.log(Hola)
+console.log(Mundo)
